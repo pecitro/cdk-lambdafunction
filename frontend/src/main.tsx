81655739c64f8930fb48125a0b825eeac3833625
yuta.odagawa <hoge@example.com>
2024-09-15 08:07:54 +0900

    a

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+import { FluentProvider, webLightTheme } from "@fluentui/react-components";
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <FluentProvider theme={webLightTheme}>
+      <App />
+    </FluentProvider>
   </StrictMode>,
-)
+);
